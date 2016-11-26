cd7eccea2533678361a16376831f55c9b500c36f
abc873693 <abc873693@gmail.com>
2016-11-26 18:17:23 +0800

    2016/11/26 ver1
    完成fragment框架第一版

---
+++ app/src/main/java/rainvisitor/personal_assistant/Drawer/NotesFragment.java
@@ -0,0 +1,92 @@
+package rainvisitor.personal_assistant.Drawer;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import rainvisitor.personal_assistant.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link NotesFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link NotesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NotesFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private TextView textView;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+
+    private OnFragmentInteractionListener mListener;
+
+    public NotesFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public NotesFragment newInstance(String param1) {
+        NotesFragment fragment = new NotesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
+
+        textView = (TextView) view.findViewById(R.id.textView);
+        textView.setText(mParam1);
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (OnFragmentInteractionListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(
+                    context.toString() + " must implement OnFragmentFInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/rainvisitor/personal_assistant/MainActivity.java
+++ app/src/main/java/rainvisitor/personal_assistant/MainActivity.java
@@ -1,10 +1,12 @@
 package rainvisitor.personal_assistant;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,14 +14,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import rainvisitor.personal_assistant.Drawer.MoneyFragment;
+import rainvisitor.personal_assistant.Drawer.NotesFragment;
+import rainvisitor.personal_assistant.Drawer.RestaurantFragment;
+import rainvisitor.personal_assistant.Drawer.SchedulesFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
+        implements NavigationView.OnNavigationItemSelectedListener,
+        NotesFragment.OnFragmentInteractionListener,
+        RestaurantFragment.OnFragmentInteractionListener,
+        SchedulesFragment.OnFragmentInteractionListener,
+        MoneyFragment.OnFragmentInteractionListener
+        {
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = (FrameLayout) findViewById(R.id.content_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,7 +44,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "請說話", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -78,24 +95,39 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.nav_note:
+                fragment = new NotesFragment().newInstance(item.getTitle().toString());
+                break;
+            case R.id.nav_schedule:
+                fragment = new SchedulesFragment().newInstance(item.getTitle().toString(),"");
+                break;
+            case R.id.nav_money_management:
+                fragment = new MoneyFragment().newInstance(item.getTitle().toString(),"");
+                break;
+            case R.id.nav_restaurant:
+                fragment = new RestaurantFragment().newInstance(item.getTitle().toString(),"");
+                break;
+            case R.id.nav_setting:
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+                break;
+            default:
+                break;
         }
-
+        if(fragment!=null){
+            FragmentTransaction fragTrans = getFragmentManager().beginTransaction();
+            fragTrans.replace(R.id.content_main,
+                    fragment);
+            fragTrans.commit();
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
     }
 }
