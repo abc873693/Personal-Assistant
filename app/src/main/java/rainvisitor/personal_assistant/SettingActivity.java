2785056fa7f9834f566cc1c977f4467b514c3bc9
alanting850420 <alanting0716@gmail.com>
2016-12-26 17:59:08 +0800

    Setting

--- app/src/main/java/rainvisitor/personal_assistant/SettingActivity.java
+++ app/src/main/java/rainvisitor/personal_assistant/SettingActivity.java
@@ -1,7 +1,12 @@
 package rainvisitor.personal_assistant;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import rainvisitor.personal_assistant.Database.SP_Service;
+import rainvisitor.personal_assistant.libs.ImageDownloaderTask;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ImageView user_image = (ImageView) findViewById(R.id.user_photo);
+        TextView user_name = (TextView) findViewById(R.id.ueser_name);
+        TextView user_email = (TextView) findViewById(R.id.user_mail);
+        SP_Service sp_service = new SP_Service(SettingActivity.this);
+        if(sp_service.getLoginState()) {
+            user_name.setText(sp_service.username_get());
+            user_email.setText(sp_service.userEmail_get());
+            new ImageDownloaderTask(user_image).execute(sp_service.userPhotoURL_get());
+        }
     }
 }
